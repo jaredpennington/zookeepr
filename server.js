4b4d73bac9777c9b24ba0a197655668be39db383
jaredpennington <94023319+jaredpennington@users.noreply.github.com>
2022-03-12 06:46:54 -0500

    hopefully this goes to github

--- server.js
+++ server.js
@@ -66,6 +66,12 @@ app.get('/api/animals', (req, res) => {
   }
 });
 
+app.post('/api/animals', (req, res) => {
+    // req.body is where our incoming content will be
+    console.log(req.body);
+    res.json(req.body);
+  });
+  
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
   });
